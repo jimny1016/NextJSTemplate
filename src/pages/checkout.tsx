0ebb9902d55dea62a34307e365cc25e64dd78ce7
Mark.K <a4756830@gmail.com>
2022-08-27 20:34:48 +0800

    update

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,42 +1,59 @@
-import Container from "@components/ui/container";
-import Layout from "@components/layout/layout";
-import Subscription from "@components/common/subscription";
-import PageHeader from "@components/ui/page-header";
-import CheckoutForm from "@components/checkout/checkout-form";
-import CheckoutCard from "@components/checkout/checkout-card";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { GetStaticProps } from "next";
+import Container from '@components/ui/container';
+import Layout from '@components/layout/layout';
+import Subscription from '@components/common/subscription';
+import PageHeader from '@components/ui/page-header';
+import CheckoutForm from '@components/checkout/checkout-form';
+import CheckoutCard from '@components/checkout/checkout-card';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { GetStaticProps } from 'next';
+import { useEffect } from 'react';
+import { checkoutViewEvent, pushSearchDataLayer } from '@utils/gtm';
 
 export default function CheckoutPage() {
-	return (
-		<>
-			<PageHeader pageHeader="text-page-checkout" />
-			<Container>
-				<div className="py-14 xl:py-20 px-0 2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto flex flex-col md:flex-row w-full">
-					<div className="md:w-full lg:w-3/5 flex  h-full flex-col -mt-1.5">
-						<CheckoutForm />
-					</div>
-					<div className="md:w-full lg:w-2/5 md:ms-7 lg:ms-10 xl:ms-14 flex flex-col h-full -mt-1.5">
-						<CheckoutCard />
-					</div>
-				</div>
-				<Subscription />
-			</Container>
-		</>
-	);
+  const gaTest = () => {
+    let text = '123';
+    pushSearchDataLayer(text);
+
+    let gtmProduct = [] as GTMProducts[];
+    checkoutViewEvent(gtmProduct);
+
+    const d = new Date();
+
+    gtmProduct.push({
+      id: d.toString(),
+      name: '123',
+      quantity: 3,
+      price: '999',
+    });
+    checkoutViewEvent(gtmProduct);
+  };
+  useEffect(() => {
+    gaTest();
+  }, []);
+  return (
+    <>
+      <PageHeader pageHeader="text-page-checkout" />
+      <Container>
+        <div className="py-14 xl:py-20 px-0 2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto flex flex-col md:flex-row w-full">
+          <div className="md:w-full lg:w-3/5 flex  h-full flex-col -mt-1.5">
+            <CheckoutForm />
+          </div>
+          <div className="md:w-full lg:w-2/5 md:ms-7 lg:ms-10 xl:ms-14 flex flex-col h-full -mt-1.5">
+            <CheckoutCard />
+          </div>
+        </div>
+        <Subscription />
+      </Container>
+    </>
+  );
 }
 
 CheckoutPage.Layout = Layout;
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-	return {
-		props: {
-			...(await serverSideTranslations(locale!, [
-				"common",
-				"forms",
-				"menu",
-				"footer",
-			])),
-		},
-	};
+  return {
+    props: {
+      ...(await serverSideTranslations(locale!, ['common', 'forms', 'menu', 'footer'])),
+    },
+  };
 };
--- typing/global.d.ts
+++ typing/global.d.ts
@@ -2,3 +2,12 @@ interface Window {
   dataLayer?: object[];
   ApplePaySession: any;
 }
+
+interface GTMProducts {
+  id: string;
+  name: string;
+  category?: string;
+  brand?: string;
+  price?: string;
+  quantity: number;
+}
