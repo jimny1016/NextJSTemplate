1b3b3019ee578e3092690ba598cb46b0aa8eb350
Mark.K <a4756830@gmail.com>
2022-08-27 11:46:23 +0800

    [Add] Project init

---
+++ .vscode/init.vscode-env.ps1
@@ -0,0 +1,25 @@
+## vs code plugin
+code --install-extension dbaeumer.vscode-eslint 
+code --install-extension stylelint.vscode-stylelint@0.87.6
+code --install-extension esbenp.prettier-vscode
+code --install-extension rvest.vs-code-prettier-eslint
+code --install-extension clinyong.vscode-css-modules
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension dzhavat.bracket-pair-toggler
+code --install-extension eamodio.gitlens
+code --install-extension mhutchie.git-graph
+code --install-extension oderwat.indent-rainbow
+code --install-extension kisstkondoros.vscode-gutter-preview
+code --install-extension OmarRwemi.BetterComments
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension alefragnani.Bookmarks
+code --install-extension pranaygp.vscode-css-peek
+## https://hackmd.io/@sam-liaw/BJnLhni7U
+cd ..
+
+## vs code plugin relation node plugin
+npm add stylelint-config-standard-scss --save -D
+npm install stylelint-config-standard-scss@5.0.0 --dev --save
+
+# npm install --save-dev eslint-config-prettier
+# npm add prettier eslint-config-prettier --save -D
