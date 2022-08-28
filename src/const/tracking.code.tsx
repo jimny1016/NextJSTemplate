eec5dda3a0037a29cd69c4635ce56c2390d482ae
Mark.K <a4756830@gmail.com>
2022-08-28 13:39:55 +0800

    update

--- src/const/tracking.code.tsx
+++ src/const/tracking.code.tsx
@@ -86,18 +86,15 @@ export namespace facebook {
 }
 
 export namespace googleTagManager {
-  const containerID = 'WGT8F6C';
+  const containerID2 = 'WGT8F6C';
+  const containerID = 'J98ZLVXRKM';
 
   export const header = (
     <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-J98ZLVXRKM"></script>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=G-${containerID}`}></script>
       <script
         dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-J98ZLVXRKM');`,
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-${containerID}');`,
         }}
       ></script>
     </>
